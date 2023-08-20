3b21f7b668b0732fc705ab5a17e7c895443cd68b
Akhtar Alzrouni <138444303+AkhtarAlzarouni@users.noreply.github.com>
2023-08-20 20:12:19 +0400

    Initial commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,39 @@
+
+import 'dart:io';
+void main(){
+  // print(12);
+  // print("Dart Course");
+  // print('Dart Course');
+
+
+  // int x=10;
+  // double y=3.9;
+  // String z= "Akhtra";
+
+  // // List<String> char = z.split('');
+  // // String a = char.join('\n');
+  // //  print(a);
+
+  // z.split('').forEach((ch) => print(ch));
+
+//   DateTime date=DateTime.now();
+//   print(date.hour);
+ 
+
+//  int? x;
+//  print(x);
+// print(x ?? 0+10);
+
+// print("please enter your name");
+// String? name=stdin.readLineSync();
+
+//
+//
+
+var list = [1, 2, 3];
+assert(list.length == 3);
+assert(list[1] == 2);
+
+list[1] = 1;
+assert(list[1] == 1);
+ }
